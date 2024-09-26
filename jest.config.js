c1916c1897251062e4345b3b95a5081a352dd62b
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2024-09-26 12:57:01 +0200

    chore: tests fix

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
-module.exports = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+  },
 };
