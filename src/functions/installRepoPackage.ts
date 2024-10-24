a3f7affce26b676d6ef629f0a4a1d3c36a101926
april <april@cyberglot.me>
2024-10-24 11:45:50 +0100

    feat: add docker setup

--- src/functions/installRepoPackage.ts
+++ src/functions/installRepoPackage.ts
@@ -1,15 +1,15 @@
-export const installDependencies = async () => {
-  // NPM install part
+export const installDependencies = (cmd: string, args: string[]) => async () => {
+  // abstracted to use either npm or docker
   console.log("Installing project dependencies...");
   await new Promise((resolve, reject) => {
     const installProcess = import("child_process").then((childProcess) =>
-      childProcess.spawn("npm", ["install"], { stdio: "inherit" }),
+      childProcess.spawn(cmd, args, { stdio: "inherit" }),
     );
     installProcess
       .then((process) => {
         process.on("close", (code: number) => {
           if (code !== 0) {
-            reject(new Error(`npm install failed with code ${code}`));
+            reject(new Error(`installation failed with code ${code}`));
           } else {
             resolve(undefined);
           }
@@ -19,3 +19,5 @@ export const installDependencies = async () => {
   });
   console.log("Dependencies installed successfully.");
 };
+
+export const installRepoPackage = installDependencies("npm", ["install"]);
