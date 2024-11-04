a57141a6db07da005b95b01f6477ebed0d7b37b1
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2024-11-04 12:14:10 +0100

    chore: revert devnet

--- examples/nextjs/app/components/ClientWrapper.tsx
+++ examples/nextjs/app/components/ClientWrapper.tsx
@@ -8,5 +8,5 @@ import { NillionProvider } from "@nillion/client-react-hooks";
 export const ClientWrapper: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  return <NillionProvider network="photon">{children}</NillionProvider>;
+  return <NillionProvider network="devnet">{children}</NillionProvider>;
 };
