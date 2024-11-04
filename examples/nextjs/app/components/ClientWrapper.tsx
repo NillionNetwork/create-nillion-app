8dd2e6b0f3018f3b687127f3e2384e852b74bf4f
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2024-11-04 12:13:45 +0100

    feat: examples

--- examples/nextjs/app/components/ClientWrapper.tsx
+++ examples/nextjs/app/components/ClientWrapper.tsx
@@ -4,8 +4,9 @@ import { ReactNode } from "react";
 
 import { NillionProvider } from "@nillion/client-react-hooks";
 
+// To switch from devnet to testnet(photon), change the network in the NillionProvider
 export const ClientWrapper: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  return <NillionProvider network="devnet">{children}</NillionProvider>;
+  return <NillionProvider network="photon">{children}</NillionProvider>;
 };
