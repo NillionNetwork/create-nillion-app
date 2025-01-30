c62f328869bd5a9ac215cbf90d6d788dd188383c
Nicholas Wehr <33910651+wwwehr@users.noreply.github.com>
2025-01-30 07:11:34 -0700

    allow all nilup versions in nilup check (#20)
    * Update isNilupInstalled.ts
    
    the existing method checks for old nilup bin version
    
    * Update isNilupInstalled.ts
    
    fixed typo from web console editor. where is vi?

--- src/functions/isNilupInstalled.ts
+++ src/functions/isNilupInstalled.ts
@@ -4,10 +4,10 @@ import { installNilup } from "./installNilup.js";
 export async function isNilupInstalled() {
   try {
     const output = execSync("nilup -V", { stdio: "pipe" }).toString().trim();
-    if (output !== "nilup v0.7.0") {
-      await installNilup();
-    } else {
+    if (output.toLowerCase().includes("nilup")) {
       console.log(`Nilup is already installed. ${output}`);
+    } else {
+      await installNilup();
     }
   } catch (error: unknown) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
